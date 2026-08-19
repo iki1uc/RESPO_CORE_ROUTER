362773be9a7b0d0420deaf4159d1497ffa2ceac1
iki1uc <entertrainer@web.de>
2026-08-19 14:04:47 +0200

    Update RESPO_UI_STATUS.js

--- RESPO_UI_STATUS.js
+++ RESPO_UI_STATUS.js
@@ -1,1 +1,10 @@
 
+// RESPO_UI_STATUS – NEUE VERSION
+// Global, stabil, kompatibel mit Router
+
+window.RESPO_UI_STATUS = {
+    update() {
+        console.log("UI‑Status aktualisiert.");
+        console.log("UI stabil.");
+    }
+};
